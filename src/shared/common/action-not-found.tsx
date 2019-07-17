cdf4e1ff97a03a94d573e7e55233f4fba1a8e686
Gyan <guptagyan58@gmail.com>
2019-07-17 14:58:42 +0530

    fixed requested changes

--- src/shared/common/action-not-found.tsx
+++ src/shared/common/action-not-found.tsx
@@ -1,15 +1,8 @@
-import Divider from "antd/lib/divider";
-import Icon from "antd/lib/icon";
 // @ts-ignore
 import { t } from "onefx/lib/iso-i18n";
 // @ts-ignore
 import Helmet from "onefx/lib/react-helmet";
 import React from "react";
-import { Flex } from "../common/flex";
-import { Navigation } from "../common/navigation";
-import { PageTitle } from "../common/page-title";
-import { colors } from "../common/styles/style-color";
-import { ContentPadding } from "../common/styles/style-padding";
 import { assetURL } from "./asset-url";
 import { ErrorPage } from "./error-page";
 
@@ -18,28 +11,11 @@ type Props = {
 };
 
 export const ActionNotFound = ({ info }: Props): JSX.Element => (
-  <>
-    <Helmet title={`${t("action.notFound")} - IoTeX`} />
-    <ContentPadding>
-      <Navigation />
-      <Flex
-        width={"100%"}
-        column={true}
-        alignItems={"baselines"}
-        backgroundColor={colors.white}
-      >
-        <PageTitle>
-          <Icon type="project" /> {t("action.action")}
-        </PageTitle>
-        <Divider orientation="left">{t("title.overview")}</Divider>
-        <ErrorPage
-          bg={assetURL("/action-not-found.png")}
-          bar={t("not_found.bar")}
-          info={t("action.notFound")}
-          title={t("not_found.title")}
-          subTitle={`${t("action.pending")} ${info}`}
-        ></ErrorPage>
-      </Flex>
-    </ContentPadding>
-  </>
+  <ErrorPage
+    bg={assetURL("/action-not-found.png")}
+    bar={t("not_found.bar")}
+    info={t("action.notFound")}
+    title={t("not_found.title")}
+    subTitle={`${t("action.pending")} ${info}`}
+  ></ErrorPage>
 );
